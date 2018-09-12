1f0aab70615bef593aeff11f73556d128b2f199e
iadler19 <42978499+iadler19@users.noreply.github.com>
2018-09-12 14:07:14 -0400

    Add files via upload

---
+++ src/SmileyFace.java
@@ -0,0 +1,21 @@
+
+/**
+ * 
+ * @author ian adler pd 7
+ * Smiley face
+ *
+ */
+public class SmileyFace {
+
+	public static void main(String[] args)
+	{
+	    System.out.println("\\\\\\\\\\\\\\");
+	    System.out.println("| * * |");
+	    System.out.println("|  *  |");
+        System.out.println( "| \\_/ |");
+        System.out.println( "\\_____/");
+        
+	}
+
+}
+
